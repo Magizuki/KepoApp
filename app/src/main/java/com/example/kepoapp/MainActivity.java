fd2c6763aab75dfb2e963ed8a75c71636abbdd40
Magizuki <irvinesenjaya@yahoo.co.id>
2021-01-24 20:32:02 +0700

    Add Assets and Splash Feature

--- app/src/main/java/com/example/kepoapp/MainActivity.java
+++ app/src/main/java/com/example/kepoapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.kepoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Splash Duration
+    private final int SPLASH_DURATION = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Splash Initialization
+        //Splash duration in millisecond
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+
+
     }
 }
